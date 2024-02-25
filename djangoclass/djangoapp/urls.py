76cdbb85f1718400aff1cb56005709b14dfa9b2d
jigarkoshiya <jigarkoshiya2001@gmail.com>
2024-02-25 12:46:47 +0530

    authentication and product manipulation

--- djangoclass/djangoapp/urls.py
+++ djangoclass/djangoapp/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import products,user_reg
+from .views import products,user_reg, user_login, user_logout, update_product
 
 
 urlpatterns = [
     # path('index/',index, name="index"),
     path('products/',products,name="products"),
     path('user_reg/',user_reg,name="user_reg"),
-    # path('user_login/',user_login,name="user_login"),
+    path('user_login/',user_login,name="user_login"),
+    path('user_logout/', user_logout, name="user_logout"),
+    path('update_product/<str:product_id>/', update_product, name="update_product"),
+
 ]
--- djangoclass/djangoapp/views.py
+++ djangoclass/djangoapp/views.py
@@ -1,9 +1,9 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Products
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
-
+from django.contrib.auth import login, logout, authenticate
 
 def products(request):
     products=Products.objects.all()
@@ -19,7 +19,7 @@ def products(request):
         "products": products, 
         "product_count": len(products)
     }
-    return render(request, 'products.html', context=context)
+    return render(request, 'product.html', context=context)
 
 
 
@@ -40,17 +40,56 @@ def user_reg(request):
         
         existing_user = User.objects.filter(username=username).first()
         print(existing_user)
+
+        existing_email = User.objects.filter(email=email).first()
+
         if existing_user:
             message = 'this user already exists'
+        elif existing_email:
+            message = 'this email already exists'
+        elif password != confirm_password:
+            message = 'password doesnt exists'
         else:
             # user = User(username=username, email=email, password=password)
             user = User.objects.create_user(username=username, email=email, password=password)
             # products = Products.objects.create()
             user.save()
-            print(username, email, password, confirm_password)
+            return redirect("user_login")
         context = {"message": message}
     return render(request, 'user_reg.html', context)
 
 
 def user_login(request):
-    pass
+    message = ""
+    if request.method == "POST":
+        csrf_token = request.POST.get('csrfmiddlewaretoken')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        username = User.objects.filter(email=email).first()
+        print(username, type(username))
+        is_authenticated = authenticate(request, username=username, password=password)
+        # if not username:
+        #     message = "Your email is not registered with us"
+        if not is_authenticated:
+            message = "Password or email doesnt matches. Email maybe not registered with us"
+        elif username:            
+            login(request, user=username)
+            print(username.username, type(username.username), "username.username")
+            return redirect('products')
+    context = {"message": message}
+    return render(request=request, template_name='user_login.html', context=context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('products')
+
+
+def update_product(request, product_id):
+    product_object = Products.objects.filter(id=product_id).first()
+    if not product_object:
+        return redirect('products')
+        
+    context = {"product_object": product_object}
+    return render(request=request, template_name='update_product.html', context=context)
